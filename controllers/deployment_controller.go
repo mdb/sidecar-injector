1a91a6bd53926478bfac31d71403ae1fd921eb54
Mike Ball <mikedball@gmail.com>
2022-11-29 16:02:51 -0500

    update permissions
    Signed-off-by: Mike Ball <mikedball@gmail.com>

--- controllers/deployment_controller.go
+++ controllers/deployment_controller.go
@@ -33,8 +33,6 @@ type DeploymentReconciler struct {
 }
 
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=apps,resources=deployments/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=apps,resources=deployments/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
