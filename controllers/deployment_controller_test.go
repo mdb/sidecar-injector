ac881e71b3d820f7cc7c74159a523ce3a22fc943
Mike Ball <mikedball@gmail.com>
2022-12-11 12:49:35 -0500

    preliminary failing test

---
+++ controllers/deployment_controller_test.go
@@ -0,0 +1,51 @@
+package controllers
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var _ = Describe("sidecar injector controller", func() {
+
+	const (
+		deploymentName = "test-deployment"
+		namespace      = "default"
+	)
+
+	Context("When a Deployment is created", func() {
+		It("Should inject a sidecar container", func() {
+			ctx := context.Background()
+			deployment := &appsv1.Deployment{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "apps/v1",
+					Kind:       "Deployment",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      deploymentName,
+					Namespace: namespace,
+				},
+				Spec: appsv1.DeploymentSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{
+							Containers: []v1.Container{
+								{
+									Name:  "test-container",
+									Image: "test-image",
+								},
+							},
+							RestartPolicy: v1.RestartPolicyOnFailure,
+						},
+					},
+				},
+			}
+
+			Expect(k8sClient.Create(ctx, deployment)).Should(Succeed())
+		})
+	})
+})
